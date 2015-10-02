d4286c0b4fb3dea1f5d771126141b56fd1a55cc2
art <art@art.com>
2015-10-01 20:54:00 -0400

    untested indirect power-measuring. -each interrupt, we calculate the average strain since the last interrupt -combined with the calculated RPM, that gives us a datapoint for speed->power -each half-second or so, we get the recent RPM data, and use a linear regression on the speed->power data to map from speed to power

--- strain_gage_shield_arduino_uno_code.ino
+++ strain_gage_shield_arduino_uno_code.ino
@@ -1,94 +1,162 @@
 // SGS Calibration by linear interpolation for Strain 1 and Strain 2
 
 // Put two known loads on the Strain Gauge sensor and write obtained values below :  (You can use Strain 1 or Strain 2 or the two Strains) 
-
-// variables for interrupt sequence
-long hitsWheel = 0;
-
-
-volatile unsigned long curMicroWheel;
+//constants:
+const int time_step = 250; // milliseconds between serial outputs
+const int kHolesPerWheel = 2;
 
 
-int wheelReadingThreshold = 2;
-float rpmTol = 1.5;
-
-
-unsigned long currentMillis = 0;
-unsigned long prevMillis = 0;
 
 unsigned long lastWheelTime = 0;
-int readingW = 0;
-int prevReadingW=0;
-float rpmW=0;
+
+unsigned long cRpmsSinceLastReport = 0;
+float flRpmSumSinceLastReport = 0;
+
+unsigned long cStrainsSinceLastReport=0;
+float flStrainSumSinceLastReport=0;
 
 
-float ReadingA_Strain1 = 301.0;
-float LoadA_Strain1 = 0.0; //  (Kg,lbs..) 
-float ReadingB_Strain1 = 310.0;
-float LoadB_Strain1 = 1.0; //  (Kg,lbs..) 
+unsigned int loops = 0;
 
-float ReadingA_Strain2 = 1.0;
-float LoadA_Strain2 = 0.0; //  (Kg,lbs..) 
-float ReadingB_Strain2 = 60.0;
-float LoadB_Strain2 = 80.0; //  (Kg,lbs..) 
+long lastPrintTime = 0;
 
+//////////////////////////////
+////////////////////////////
+// linear regression for speed->power
 
-int time_step = 10 ; // reading every 2.5s
-long time = 0;
+#ifdef _MSC_VER
+typedef int int32;
+#else
+typedef long long int32;
+#endif
+
+struct HistoryData {
+  const static int32 kCount = 5;
+  float rgSpeeds[kCount];
+  float rgValues[kCount];
+  int cForcePoints;
+  int ixUpdatePoint;
+  float flLastSlope;
+};
+
+HistoryData historyCounter;
+
+int InitHistoryData()
+{
+  memset(&historyCounter, 0, sizeof(historyCounter));
+}
+
+float CalculateSlope()
+{
+  float sum_yixi = 0;
+  float sum_xi2 = 0;
+  for(int x = 0;x < historyCounter.cForcePoints; x++)
+  {
+    sum_yixi += historyCounter.rgSpeeds[x]*historyCounter.rgValues[x];
+    sum_xi2 += historyCounter.rgSpeeds[x]*historyCounter.rgSpeeds[x];
+  }
+
+  if(sum_xi2 != 0)
+  {
+    return sum_yixi / sum_xi2;
+  }
+  return 0;
+}
+void HandleDataReport(float flWheelSpeed, float flValue)
+{
+  historyCounter.rgSpeeds[historyCounter.ixUpdatePoint] = flWheelSpeed;
+  historyCounter.rgValues[historyCounter.ixUpdatePoint] = flValue;
+  historyCounter.ixUpdatePoint = (historyCounter.ixUpdatePoint + 1) % HistoryData::kCount;
+
+  historyCounter.cForcePoints = max(historyCounter.cForcePoints, historyCounter.ixUpdatePoint);
+
+}
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+
 
 void setup() {
   Serial.begin(115200); //  setup serial baudrate
-      attachInterrupt(0, countWheel, FALLING);
+  attachInterrupt(0, countWheel, FALLING);
 
-
+  InitHistoryData();
 }
 
-void loop() {
-  float newReading_Strain1 = analogRead(0);  // analog in 0 for Strain 1
-  float newReading_Strain2 = analogRead(1);  // analog in 1 for Strain 2
-  int newReading_hall = digitalRead(2);
+float getCurrentStrain()
+{
+  const float ReadingA_Strain1 = 297.8;
+  const float LoadA_Strain1 = 0.0; //  (Kg,lbs..) 
+  const float ReadingB_Strain1 = 383.5;
+  const float LoadB_Strain1 = 5; //  (Kg,lbs..) 
+  const float slopeCalibration =  ((LoadB_Strain1 - LoadA_Strain1)/(ReadingB_Strain1 - ReadingA_Strain1));
+
+  const float newReading_Strain1 = analogRead(0);  // analog in 0 for Strain 1
   // Calculate load by interpolation 
-  float load_Strain1 = ((LoadB_Strain1 - LoadA_Strain1)/(ReadingB_Strain1 - ReadingA_Strain1)) * (newReading_Strain1 - ReadingA_Strain1) + LoadA_Strain1;
-  float load_Strain2 = ((LoadB_Strain2 - LoadA_Strain2)/(ReadingB_Strain2 - ReadingA_Strain2)) * (newReading_Strain2 - ReadingA_Strain2) + LoadA_Strain2;
+  const float load_Strain1 = slopeCalibration * (newReading_Strain1 - ReadingA_Strain1) + LoadA_Strain1;
+  
+  return newReading_Strain1;
+}
+
+void loop() 
+{
+  loops++;
+  
+  flStrainSumSinceLastReport += getCurrentStrain();
+  cStrainsSinceLastReport++;
   
   // millis returns the number of milliseconds since the board started the current program
-  if(millis() > time_step+time) {
-    //Serial.print("Reading_Strain1 : ");
-    //Serial.print(newReading_Strain1);     // display strain 1 reading
-    //Serial.print("  Load_Strain1 : ");
-    Serial.print(time);
-        Serial.print(" , ");
-    Serial.println(load_Strain1);     // display strain 1 load
-    //Serial.print(" RPM: ");
-    //Serial.println(rpmW);
-//    Serial.print("Reading_Strain2 : ");
-//    Serial.print(newReading_Strain2);     // display strain 2 reading
-//    Serial.print("  Load_Strain2 : ");
-//    Serial.println(load_Strain2);         // display strain 2 load 
-//    Serial.println('\n');
-    time = millis();
+  const long tmNow = millis();
+  if(tmNow > time_step+lastPrintTime && cRpmsSinceLastReport > 0) {
+    
+    // get the RPM, convert RPM to power
+    float flRPMNow = flRpmSumSinceLastReport / cRpmsSinceLastReport;
+    flRpmSumSinceLastReport = 0;
+    cRpmsSinceLastReport = 0;
+    
+    float flSlopeNow = CalculateSlope();
+    float flImpliedPower = flRPMNow * flSlopeNow;
+    
+    
+    Serial.print(loops);
+    Serial.print("\t");
+    Serial.print(tmNow);
+    Serial.print("\t");
+    Serial.print(flRPMNow);
+    Serial.print("\t");
+    Serial.println(flImpliedPower);
+    Serial.print("\t");
+    Serial.println(flSlopeNow);
+    Serial.print("\t");
+    Serial.println(historyCounter.cForcePoints);
+    Serial.print("\t");
+    Serial.println(historyCounter.ixUpdatePoint);
+    lastPrintTime = tmNow;
   }
 }
-//interrupt counters, could also trigger timer measurement here for more accurate RPM
 void countWheel()
 {
- hitsWheel++;
- if(hitsWheel == wheelReadingThreshold)
- {
-     curMicroWheel = micros();
-    Serial.println("Wheel hit");
-     const int kHolesPerWheel = 1;
-     unsigned long dtW = curMicroWheel-lastWheelTime;
-     lastWheelTime = curMicroWheel;
-     long oldWheelRPM = rpmW;
-     rpmW = wheelReadingThreshold*60./(dtW/1000000.);
-     rpmW /= kHolesPerWheel;
-     hitsWheel = 0;
-     
-//     if(rpmW/oldWheelRPM>(rpmTol)&&rpmW>=15)
-//     {
-//        rpmW = oldWheelRPM; 
-//     }
-     
- }
+    const unsigned long curMicroWheel = micros();
+    
+    const unsigned long dtW = curMicroWheel-lastWheelTime;
+    lastWheelTime = curMicroWheel;
+    
+    if(dtW > 50000) // >50000 so we don't get spurious readings
+    {
+      float rpmW = 60.*1000000./((float)dtW);
+      rpmW /= kHolesPerWheel;
+      
+      const float flStrainNowKg = flStrainSumSinceLastReport / cStrainsSinceLastReport;
+      cStrainsSinceLastReport = 0;
+      flStrainSumSinceLastReport = 0;
+      const float flStrainNowN = flStrainNowKg*9.8;
+      const float flSpeedNowMs = rpmW / 60 * 0.7*3.14159;
+      const float flPowerNow = flStrainNowN*flSpeedNowMs;
+      
+      
+      HandleDataReport(rpmW, flPowerNow);
+      
+      cRpmsSinceLastReport++;
+      flRpmSumSinceLastReport += rpmW;
+    }
 }
